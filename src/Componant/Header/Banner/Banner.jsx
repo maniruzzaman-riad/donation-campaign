ec21ae530baaec1cc576e669f9dfde351528f6a1
maniruzzaman-riad <monirozzamanr94@gmail.com>
2023-09-27 22:55:00 +0600

    Banner section complitely done and make responsive

--- src/Componant/Header/Banner/Banner.jsx
+++ src/Componant/Header/Banner/Banner.jsx
@@ -8,8 +8,20 @@ const Banner = () => {
                 <div className="w-full h-full bg-white absolute top-0 opacity-80">
 
                 </div>
+                <div className="absolute top-0 md:top-[15%] lg:top-[40%] md:left-[9%] lg:left-[25%]">
+                    <h2 className="text-2xl md:text-4xl font-bold text-center mb-5">I Grow By Helping People In Need</h2>
+                    <div>
+                        <form>
+                            <div className="flex justify-center">
+                                <input className="border py-2 px-3 text-base rounded-l-md w-3/4 md:w-3/5 lg:w-3/5" type="text" name="" id="" placeholder="Search here...." />
+                                <button className="text-base font-semibold text-white py-2 px-5 border border-[#FF444A] bg-[#FF444A] rounded-r-md">Search</button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
             </div>
-            <h2 className="text-4xl font-bold text-center">I Grow By Helping People In Need</h2>
+
         </div>
     );
 };
--- src/Componant/Home/Home.jsx
+++ src/Componant/Home/Home.jsx
@@ -1,14 +1,19 @@
+import { createContext } from "react";
 import Donation from "../Donation/Donation";
 import Banner from "../Header/Banner/Banner";
 
 
+const AssetContext = createContext()
+
 const Home = () => {
     return (
         <div>
-            <Banner></Banner>
-            <div>
-                <Donation></Donation>
-            </div>
+            <AssetContext.Provider>
+                <Banner></Banner>
+                <div>
+                    <Donation></Donation>
+                </div>
+            </AssetContext.Provider>
         </div>
     );
 };
